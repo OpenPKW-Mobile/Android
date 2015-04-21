a974b509204da058ca2f99ed3fcab3e939c2a89d
wradzioch <poczta@wrweb.pl>
2015-04-21 21:15:29 +0200

    Dodanie splasha, ikony aplikacji, OPM-U-1

---
+++ app/src/main/java/pl/openpkw/openpkwmobile/views/SplashActivity.java
@@ -0,0 +1,39 @@
+package pl.openpkw.openpkwmobile.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import pl.openpkw.openpkwmobile.R;
+import pl.openpkw.openpkwmobile.fragments.SplashFragment;
+
+/**
+ * Created by Wojciech Radzioch on 2015-04-21.
+ */
+public class SplashActivity extends FragmentActivity {
+
+    public static final int SPLASH_TIME = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new SplashFragment())
+                    .commit();
+        }
+
+        Thread splashThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(SPLASH_TIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent loginIntent = new Intent(SplashActivity.this, TestActivity.class);
+                startActivity(loginIntent);
+            }
+        };
+        splashThread.start();
+    }
+}
