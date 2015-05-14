6a05e2372af0858c153ec2e6d0bfc114aa8655e4
wradzioch <poczta@wrweb.pl>
2015-05-14 13:26:46 +0200

    Dodanie obsługi błędu przy rejestracji

---
+++ app/src/main/java/pl/openpkw/openpkwmobile/network/exceptions/InternalServerError.java
@@ -0,0 +1,10 @@
+package pl.openpkw.openpkwmobile.network.exceptions;
+
+/**
+ * Created by michalu on 01.05.15.
+ */
+public class InternalServerError extends Exception {
+    public InternalServerError(String message) {
+        super(message);
+    }
+}
