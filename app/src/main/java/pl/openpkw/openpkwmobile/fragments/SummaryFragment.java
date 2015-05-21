69605f9c3677f87389acbaf84fd18a31611b7ca0
kuta <bardzo@wredyn.net>
2015-05-22 00:09:41 +0200

    OPM-U-18 dodano tryb niepowodzenia w aktywnoci podsumowania

--- app/src/main/java/pl/openpkw/openpkwmobile/fragments/SummaryFragment.java
+++ app/src/main/java/pl/openpkw/openpkwmobile/fragments/SummaryFragment.java
@@ -39,7 +39,7 @@ public class SummaryFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_splash, container, false);
+        View view = inflater.inflate(R.layout.fragment_summary, container, false);
 
         info = (TextView) view.findViewById(R.id.summary_info);
         detail = (TextView) view.findViewById(R.id.summary_detail);
